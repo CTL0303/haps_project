1fb933d45ffc4e200ba7c1c4d238790cb7c67dd4
CTL0303 <128975761+CTL0303@users.noreply.github.com>
2023-04-13 14:33:18 +0800

    Delete 1.RTL_simulation/UC_LED_DEMO_runtime directory

--- 1.RTL_simulation/UC_LED_DEMO_runtime/system/connectivity.tcl
+++
@@ -1,8 +0,0 @@
-# It defines connectivity structure of HAPS boards. 
-# @W: WARINING: The BREAK_OUT BOARD and its associated CON_CABLE connections has been commented out for con_speed testing.
-
-
-
-
-connectPorts	  $FB1.A1  $FB1.B19
-connectPorts	  $FB1.A2  $FB1.B20
